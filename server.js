723f43aa53de23fcfa4b00ab61e8a93dc91f59d1
vzdrizhni <vzdrizhni@gmail.com>
2021-02-01 19:15:15 +0500

    Connect project to a database

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+
+const connectDB = require('./config/db')
+
+dotenv.config({path: './config/config.env'})
+
+connectDB();
 
 //routes
 const bootcamps = require('./routes/bootcamps')
 
-dotenv.config({path: './config/config.env'})
-
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('combined'))
+}
 
 app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log('server running on port ' + PORT))
+const server = app.listen(PORT, console.log('server running on port '.green + PORT.yellow.bold));
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log('Error' + err.message);
+    server.close(() => {
+        process.exit(1);
+    });
+})
