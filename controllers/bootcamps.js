eec59a38e2e9da0168ddcd5f864a83ea2b08fd00
vzdrizhni <vzdrizhni@gmail.com>
2021-02-01 20:48:59 +0500

    Add post creation and save it in a database

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,5 @@
+const Bootcamp = require('../models/Bootcamp');
+
 exports.getBootcamps = (req, res, next) => {
     res.status(200).json({msg: 'Successfully fetched all posts'})
 }
@@ -6,8 +8,13 @@ exports.getBootcamp = (req, res, next) => {
     res.status(200).json({msg: 'Successfully post' + req.params.id})
 }
 
-exports.crateBootcamp = (req, res, next) => {
-    res.status(200).json({msg: 'Successfully created post' + req.params.id})
+exports.crateBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.create(req.body);
+        res.status(200).json({message: 'Success', data: bootcamp})
+    } catch (error) {
+        res.status(400).json({success: flase});
+    }
 }
 
 exports.updateBootcamp = (req, res, next) => {
--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const bootcamps = require('./routes/bootcamps')
 
 const app = express();
 
+//body parser
+
+app.use(express.json());
+
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('combined'))
 }
