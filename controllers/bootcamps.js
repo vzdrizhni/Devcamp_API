77a82e3d55e26f25ca3adf50b05af2638659813f
vzdrizhni <vzdrizhni@gmail.com>
2021-02-02 17:07:21 +0500

    Add errors validation

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,52 +1,63 @@
 const Bootcamp = require('../models/Bootcamp');
+const ErrorResponse = require('../utils/errorResponse');
 
-exports.getBootcamps = async (req, res, next) => {
-    try {
-        const bootcamps = await Bootcamp.find()
-        res.status(200).json({success: true, data: bootcamps})
-    } catch (error) {
-        res.status(400).json({success: false})
-    }
+exports.getBootcamps = async(req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find()
+    res
+      .status(200)
+      .json({success: true, data: bootcamps})
+  } catch (error) {
+    next(error)
+  }
 }
 
-exports.getBootcamp = async (req, res, next) => {
-    try {
-        const bootcamp = await Bootcamp.findById(req.params.id);
-        res.status(200).json({success: true, data: bootcamp})
-        if (!bootcamp) {
-            res.status(404)
-        }
-    } catch (error) {
-        next(error)
-    }
-}
+exports.getBootcamp = async(req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
 
-exports.crateBootcamp = async (req, res, next) => {
-    try {
-        const bootcamp = await Bootcamp.create(req.body);
-        res.status(200).json({message: 'Success', data: bootcamp})
-    } catch (error) {
-        res.status(400).json({success: flase});
-    }
-}
-
-exports.updateBootcamp = async (req, res, next) => {
-    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true
-    });
     if (!bootcamp) {
-        return res.status(404).json({success: false})
+      return next(new ErrorResponse(`Bootcamp not found with id ${req.params.id}`, 404))
     }
 
-    res.status(200).json({success: true, data: bootcamp})
+    res
+    .status(200)
+      .json({success: true, data: bootcamp})
+  } catch (error) {
+    next(error)
+  }
 }
 
-exports.deleteBootcamp = async (req, res, next) => {
-    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
-    if (!bootcamp) {
-        return res.status(404).json({success: false})
-    }
+exports.crateBootcamp = async(req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+    res
+      .status(200)
+      .json({message: 'Success', data: bootcamp})
+  } catch (error) {
+    next(error)
+  }
+}
 
-    res.status(200).json({success: true, data: bootcamp})
+exports.updateBootcamp = async(req, res, next) => {
+  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+  if (!bootcamp) {
+    return next(new ErrorResponse(`Bootcamp not found with id ${req.params.id}`, 404))
+  }
+
+  next(error)
+}
+
+exports.deleteBootcamp = async(req, res, next) => {
+  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  if (!bootcamp) {
+    return next(new ErrorResponse(`Bootcamp not found with id ${req.params.id}`, 404))
+  }
+
+  res
+    .status(200)
+    .json({success: true, data: bootcamp})
 }
--- middleware/error.js
+++ middleware/error.js
@@ -1,8 +1,29 @@
-const colors = require('colors');
+const ErrorResponse = require('../utils/errorResponse');
 
 const errorHandler = (err, req, res, next) => {
-    console.log(err.stack.red);
-    res.status(500).json({success: false, error: err.message})
+    let error = {...err};
+    error.message = err.message;
+
+    console.log(err.message);
+
+    if (err.name === 'CastError') {
+        console.log('gotit');
+        const message = `Bootcamp not found with id ${err.value}`;
+        error = new ErrorResponse(message, 404);
+    }
+
+    if (err.code === 11000) {
+        const message = 'Duplicate field value entered';
+        error = new ErrorResponse(message, 400);
+    }
+
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(val => val.message);
+        console.log(message);
+        error = new ErrorResponse(message, 400);
+    }
+
+    res.status(error.statusCode || 500).json({success: false, error: err.message || 'Srver Error'})
 }
 
 module.exports = errorHandler;
