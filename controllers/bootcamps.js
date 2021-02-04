ba62c0cd2814e07df7960a0529f03178a0ce4751
vzdrizhni <vzdrizhni@gmail.com>
2021-02-04 16:07:03 +0500

    Add pagination and another couple of filters

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -6,16 +6,65 @@ const geocoder = require('../utils/geocoder');
 exports.getBootcamps = asyncHandler(async(req, res, next) => {
   let query;
 
-  let queryString = JSON.stringify(req.query)
+  const reqQuery = {
+    ...req.query
+  };
 
-  queryString =queryString.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+  console.log(reqQuery);
 
-  query = Bootcamp.find(queryString);
+  const removeFields = ['select', 'sort', 'page', 'limit'];
+
+  removeFields.forEach(param => delete reqQuery[param]);
+
+  let queryString = JSON.stringify(reqQuery);
+
+  queryString = queryString.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+  console.log(queryString);
+
+  query = Bootcamp.find(JSON.parse(queryString));
+
+  if (req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields);
+  }
+
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt')
+  }
+
+  //PAgination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 1;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total =  await Bootcamp.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit
+    }
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit
+    }
+  }
 
   const bootcamps = await query;
   res
     .status(200)
-    .json({success: true, data: bootcamps})
+    .json({success: true, data: bootcamps, pagination: pagination})
 
 })
 
@@ -34,12 +83,12 @@ exports.getBootcamp = asyncHandler(async(req, res, next) => {
 })
 
 exports.crateBootcamp = asyncHandler(async(req, res, next) => {
-  
-    const bootcamp = await Bootcamp.create(req.body);
-    res
-      .status(200)
-      .json({message: 'Success', data: bootcamp})
-  
+
+  const bootcamp = await Bootcamp.create(req.body);
+  res
+    .status(200)
+    .json({message: 'Success', data: bootcamp})
+
 })
 
 exports.updateBootcamp = asyncHandler(async(req, res, next) => {
@@ -65,26 +114,32 @@ exports.deleteBootcamp = asyncHandler(async(req, res, next) => {
     .json({success: true, data: bootcamp})
 })
 
-exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
-  const { zipcode, distance } = req.params;
+exports.getBootcampsInRadius = asyncHandler(async(req, res, next) => {
+  const {zipcode, distance} = req.params;
 
   // Get lat/lng from geocoder
   const loc = await geocoder.geocode(zipcode);
   const lat = loc[0].latitude;
   const lng = loc[0].longitude;
 
-  // Calc radius using radians
-  // Divide dist by radius of Earth
-  // Earth Radius = 3,963 mi / 6,378 km
+  // Calc radius using radians Divide dist by radius of Earth Earth Radius = 3,963
+  // mi / 6,378 km
   const radius = distance / 3963;
 
   const bootcamps = await Bootcamp.find({
-    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
+    location: {
+      $geoWithin: {
+        $centerSphere: [
+          [
+            lng, lat
+          ],
+          radius
+        ]
+      }
+    }
   });
 
-  res.status(200).json({
-    success: true,
-    count: bootcamps.length,
-    data: bootcamps
-  });
+  res
+    .status(200)
+    .json({success: true, count: bootcamps.length, data: bootcamps});
 });
