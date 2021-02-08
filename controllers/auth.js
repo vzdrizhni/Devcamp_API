0faf1dd44d416833cee28a81d58d3b9751d1bbc7
vzdrizhni <vzdrizhni@gmail.com>
2021-02-08 15:03:05 +0500

    Add user login

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,3 +15,29 @@ exports.register = asyncHandler(async(req, res, next) => {
 
   res.json({data: user, token});
 });
+
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate emil & password
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide an email and password', 400));
+  }
+
+  // Check for user
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // sendTokenResponse(user, 200, res);
+  res.json({data: user})
+});
