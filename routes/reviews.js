afed620ee59a0d0a7846c469abc86e4798b2632c
vzdrizhni <vzdrizhni@gmail.com>
2021-02-10 15:52:44 +0500

    Add controller for creating reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {getReviews} = require('../controllers/reviews');
+const {getReviews, getReview, addReview} = require('../controllers/reviews');
 
 const Review = require('../models/Review');
 
@@ -14,10 +14,11 @@ router
     path: 'bootcamp',
     select: 'name description'
   }), getReviews)
+  .post(protect, authorize('user', 'admin'), addReview)
 
-// router
-//   .route('/:id')
-//   .get(getReview)
+router
+  .route('/:id')
+  .get(getReview)
 //   .put(protect, authorize('user', 'admin'), updateReview)
 //   .delete(protect, authorize('user', 'admin'), deleteReview);
 
--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
 app.use('/api/v1/auth', auth);
 app.use('/api/v1/users', users);
-app.use('./reviews', reviews);
+app.use('/api/v1/reviews', reviews);
 
 app.use(errorHandler);
 
