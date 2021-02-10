e1874263c0f155702253b3daf9e33c05381fca04
vzdrizhni <vzdrizhni@gmail.com>
2021-02-10 20:02:38 +0500

    Finish application protection

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const colors = require('colors');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 
 const errorHandler = require('./middleware/error');
 
@@ -38,6 +43,20 @@ if (process.env.NODE_ENV === 'development') {
 app.use(fileupload());
 
 app.use(mongoSanitize());
+
+app.use(helmet());
+
+app.use(xss());
+
+const limiter = rateLimit({
+    windowMs: 1000 * 10 * 60,
+    max: 100
+})
+app.use(limiter);
+
+app.use(hpp());
+
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
